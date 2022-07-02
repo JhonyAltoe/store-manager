71b6e87e5405a35ca6eb3a077c09c6dbee9a6de2
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-07-01 23:01:18 -0300

    feat(routes/productRoutes): adiciona a rota post /products da função registerProduct

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,4 +6,6 @@ const productRoutes = express.Router();
 productRoutes.get('/', controllers.product.getAllProducts);
 productRoutes.get('/:id', controllers.product.getProductById);
 
+productRoutes.post('/', controllers.product.registerProduct);
+
 module.exports = productRoutes;
