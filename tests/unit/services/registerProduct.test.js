e353b06714fe8b60e2e27d1e42f6bad0fa379579
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-07-02 14:13:27 -0300

    test(tests/.../registerProduct.test.js): faz testes para as validações da função registerProduct.js dentro do service
    É feito o teste para verivicar se retorna um erro quando o produto vem vazio ou vem com tamanho menor que 5

--- tests/unit/services/registerProduct.test.js
+++ tests/unit/services/registerProduct.test.js
@@ -27,4 +27,38 @@ describe('Testa a função "registerProduct" da camada service', () => {
       expect(response).to.deep.equal(PAYLOAD_PRODUCT_RESPONSE);
     });
   });
+
+  describe('Quando o retorno é mal sucedido', () => {
+    const PRODUCT_EMPTY = {};
+
+    const PRODUCT_LENGTH_4 = { name: 'Prod' };
+
+    before(async () => {
+      sinon.stub(models.product, 'registerProduct').resolves();
+    });
+
+    after(async () => {
+      models.product.registerProduct.restore();
+    });
+
+    it('retorn um erro quando o produto vem vazio com a menssagem \'"name" is required\' e código 400', async () => {
+      try {
+        const response = await services.product.registerProduct(PRODUCT_EMPTY);
+        expect(response).to.be(null);
+      } catch (err) {
+        expect(err.message).to.be.equal("\"name\" is required");
+        expect(err.statusCode).to.be.equal(400);
+      }
+    });
+
+    it('retorn um erro quando o produto vem vazio com a menssagem \'"name" length must be at least 5 characters long\' e código 422', async () => {
+      try {
+        const response = await services.product.registerProduct(PRODUCT_LENGTH_4);
+        expect(response).to.be(null);
+      } catch (err) {
+        expect(err.message).to.be.equal('"name" length must be at least 5 characters long');
+        expect(err.statusCode).to.be.equal(422);
+      }
+    });
+  });
 });
