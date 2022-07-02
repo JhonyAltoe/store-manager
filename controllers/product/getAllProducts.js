cce6c46450d33f91c91a591bbaade222237d9114
Jhony Altoé <jhonymikealtoe@hotmail.com>
2022-07-02 12:26:14 -0300

    refactor(cotroller/product/getAllProducts.js): agora uso o next para tratar os erros
    Agora usa o next para tratar os erros e manter o padrão da aplicação onde todos os erros vão precisar de passar pelo next. Nesse momento não foram feitos testes para essa nova linha de código.
    
    BREAKING CHANGE: Esse código quebra o teste na pasta de testes unitários dentro da pasta controller

--- controllers/product/getAllProducts.js
+++ controllers/product/getAllProducts.js
@@ -1,12 +1,12 @@
 const services = require('../../services');
 
-const getAllProducts = async (_req, res) => {
+const getAllProducts = async (_req, res, next) => {
   try {
     const allProducts = await services.product.getAllProducts();
 
     return res.status(200).json(allProducts);
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
